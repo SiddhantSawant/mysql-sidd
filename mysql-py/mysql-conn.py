69393cc6ad5baa18c97f77ba2a02604dd1404fc0
SiddhantSawant <siddhu2108@gmail.com>
2017-06-13 10:51:47 -0400

    Commiting mysql-connection code for review

--- mysql-py/mysql-conn.py
+++ mysql-py/mysql-conn.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python
-
 hostname = '127.0.0.1'
 username = 'root'
 password =  'redhat'
 database = 'mysql'
-#socket ='/tmp/mysql-master5520.sock'
 
-# Simple routine to run a query on a database and print the results:
 def Query( conn ) :
     print "Printing user and hosts from the box\n"
     cur = conn.cursor()
     cur.execute( "SELECT user,host FROM user" )
-#   for row in cur :
     row = cur.fetchone()
     while row is not None:
         print row[0], row[1]
         row = cur.fetchone()
-#       print (row)
 
 def index ( conn1 ) :
     cur = conn1.cursor()
@@ -24,7 +19,6 @@ def index ( conn1 ) :
     cur.execute( "SELECT count(*) tables,concat(round(sum(table_rows)/1000000,2),'M') rows,concat(round(sum(data_length)/(1024*1024*1024),2),'G') data,concat(round(sum(index_length)/(1024*1024*1024),2),'G') idx,concat(round(sum(data_length+index_length)/(1024*1024*1024),2),'G') total_size,round(sum(index_length)/sum(data_length),2) idxfrac FROM information_schema.TABLES;")
     for row in cur :
         print (row)
-
 
 def data_big ( conn3 ) :
     cur = conn3.cursor()
@@ -40,8 +34,7 @@ def data_dist ( conn4 ) :
    for row in cur :
        print (row)
 
-print "======Using pymysql========"
-print "     "
+
 import pymysql as my
 connect = my.connect( host=hostname, user=username, passwd=password, db=database )
 Query( connect )
